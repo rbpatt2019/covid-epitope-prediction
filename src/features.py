2b713a3fd6e1f70656f01d6d20b757f71540baea
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-08-26 11:27:58 -0400

    Remove index write

--- src/features.py
+++ src/features.py
@@ -15,8 +15,8 @@ data_test = pd.read_csv(Path(base_in, "input_covid.csv"))
 # To get a quick and dirt pipeline running, let's go numeric features only
 data_train.insert(5, "protein_len", data_train["protein_seq"].map(len))
 data_train.insert(5, "peptide_len", data_train["peptide_seq"].map(len))
-data_train.iloc[:, 5:].to_csv(Path(base_out, "training.csv"))
+data_train.iloc[:, 5:].to_csv(Path(base_out, "training.csv"), index=False)
 
 data_test.insert(5, "protein_len", data_test["protein_seq"].map(len))
 data_test.insert(5, "peptide_len", data_test["peptide_seq"].map(len))
-data_test.iloc[:, 5:].to_csv(Path(base_out, "testing.csv"))
+data_test.iloc[:, 5:].to_csv(Path(base_out, "testing.csv"), index=False)
