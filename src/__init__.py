e563e723b151a1cddda8655ad6d8f5a02ef04c30
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-08-26 07:51:41 -0400

    Code skeleton

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+"""This folder contains the scripts necessary for replicating analyses.
+
+They are:
+    clean.py - Make the data clean (merge datasets, fill NAs, data formatting)
+    features.py - Make features from given data
+    model.py - Make and train a model
+    evaulate.py - Make predictions and get metrics on performance
+
+Reusable parametres are specified in params.yaml
+
+All steps were executed using `dvc run`, so the analysis can be reproduced using
+`dvc repro`
+"""
