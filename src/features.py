91d208ff30e1b70397c3bcca49151706f55dc483
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-08-26 11:25:38 -0400

    Flesh out features step

---
+++ src/features.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+import pandas as pd
+
+base_in = Path("data", "clean")
+base_out = Path("data", "featured")
+
+base_out.mkdir(exist_ok=True)
+
+data_train = pd.read_csv(Path(base_in, "input_bcell_sars.csv"))
+data_test = pd.read_csv(Path(base_in, "input_covid.csv"))
+
+# Add protein and peptide length features
+# To get a quick and dirt pipeline running, let's go numeric features only
+data_train.insert(5, "protein_len", data_train["protein_seq"].map(len))
+data_train.insert(5, "peptide_len", data_train["peptide_seq"].map(len))
+data_train.iloc[:, 5:].to_csv(Path(base_out, "training.csv"))
+
+data_test.insert(5, "protein_len", data_test["protein_seq"].map(len))
+data_test.insert(5, "peptide_len", data_test["peptide_seq"].map(len))
+data_test.iloc[:, 5:].to_csv(Path(base_out, "testing.csv"))
