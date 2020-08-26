c44cab83e0ef79294635839877d054435b53f048
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-08-26 09:29:27 -0400

    Create file path if it does not exist

--- src/clean.py
+++ src/clean.py
@@ -4,22 +4,22 @@ consistent d-types, consistent formatting, etc. The only immediately obviously
 cleaning necessary is to join input_bcell.csv with input_sars.csv, as they are
 both considered training data
 """
-import os
 import shutil
+from pathlib import Path
 
 import pandas as pd
 
-base_in = os.path.join("data", "raw")
-base_out = os.path.join("data", "clean")
+base_in = Path("data", "raw")
+base_out = Path("data", "clean")
 
-data_bcell = pd.read_csv(os.path.join(base_in, "input_bcell.csv"))
-data_sars = pd.read_csv(os.path.join(base_in, "input_sars.csv"))
+base_out.mkdir(exist_ok=True)
+
+data_bcell = pd.read_csv(Path(base_in, "input_bcell.csv"))
+data_sars = pd.read_csv(Path(base_in, "input_sars.csv"))
 
 pd.concat([data_bcell, data_sars], axis=0).reset_index(drop=True).to_csv(
-    os.path.join(base_out, "input_bcell_sars.csv"), index=False
+    Path(base_out, "input_bcell_sars.csv"), index=False
 )
 
 # Test data requires no cleaning, so it is simply copied to data/clean
-shutil.copy(
-    os.path.join(base_in, "input_covid.csv"), os.path.join(base_out, "input_covid.csv")
-)
+shutil.copy(Path(base_in, "input_covid.csv"), Path(base_out, "input_covid.csv"))
