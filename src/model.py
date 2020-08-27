89adae4af355aaeaaa30936c00780480c28b827b
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-08-27 04:32:33 -0400

    Add seed to model.py

--- src/model.py
+++ src/model.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
 
+import numpy as np
+import yaml
 from joblib import dump
 from sklearn.naive_bayes import GaussianNB
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PowerTransformer
+
+# Set seed
+with open("params.yaml", "r") as file:
+    params = yaml.safe_load(file)
+    seed = params["seed"]
+
+np.random.seed(seed)
 
 # Guick and dirty - GaussianNB with PowerTransformer
 scl = PowerTransformer()
