1c73815abcdaa07d73107858bce1a8d3b0882257
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-08-28 06:29:18 -0400

    Refactor curve statistics

--- src/evaluate.py
+++ src/evaluate.py
@@ -50,31 +50,40 @@ skf = StratifiedKFold(n_splits=cv, shuffle=False)
 for train_idx, test_idx in skf.split(X, y):
     X_train, y_train = X[train_idx], y[train_idx]
     X_test, y_test = X[test_idx], y[test_idx]
-
     start = time.time()
     pl.fit(X_train, y_train)
     end = time.time()
     train_time.append(end - start)
-
     y_pred = pl.predict(X_test)
     y_prob = pl.predict_proba(X_test)[:, 1]
-
     prec, rcl, fsc, _ = precision_recall_fscore_support(
         y_test, y_pred, average="binary"
     )
     precision.append(prec)
     recall.append(rcl)
     fscore.append(fsc)
-
     prec_curve, rcl_curve, _ = precision_recall_curve(y_test, y_prob)
-    precision_curve.append(prec_curve)
-    recall_curve.append(rcl_curve)
-
+    precision_curve.append(prec_curve[::-1])
+    recall_curve.append(rcl_curve[::-1])
     fpr, tpr, _ = roc_curve(y_test, y_prob)
     tpr_roc.append(tpr)
     fpr_roc.append(fpr)
 
+# Compute a few additional metrics
 auc_score = [auc(x, y) for x, y in zip(fpr_roc, tpr_roc)]
+
+# precision_recall_curve and roc_curve both return varying numbers of points.
+# To insure equal length for averaging, interpolate over average length
+roc_len = int(mean([len(x) for x in tpr_roc]))
+pr_len = int(mean([len(x) for x in precision_curve]))
+
+tpr_roc = [
+    np.interp(np.linspace(0, 1, roc_len), x, y) for x, y in zip(fpr_roc, tpr_roc)
+]
+precision_curve = [
+    np.interp(np.linspace(0, 1, pr_len), x, y)
+    for x, y in zip(recall_curve, precision_curve)
+]
 
 # Now dump metrics
 metrics = {
@@ -89,16 +98,20 @@ metrics = {
     "Mean Training Time": mean(train_time),
     "St.Dev. Training Time": stdev(train_time),
 }
-json.dump(metrics, Path("results", "metrics.json"), indent=2)
+with open(Path("results", "metrics.json"), "w") as file:
+    json.dump(metrics, file, indent=2)
 
 # And plots
 pr_curve = pd.DataFrame(
-    {"Recall": np.mean(recall_curve), "Precision": np.mean(precision_curve)}
+    {"Recall": np.linspace(0, 1, pr_len), "Precision": np.mean(precision_curve)}
 )
 pr_curve.to_csv(Path("results", "pr_curve.csv"), index=False)
 
 roc_curve = pd.DataFrame(
-    {"False Positive Rate": np.mean(fpr_roc), "True Positive Rate": np.mean(tpr_roc)}
+    {
+        "False Positive Rate": np.linspace(0, 1, roc_len),
+        "True Positive Rate": np.mean(tpr_roc),
+    }
 )
 roc_curve.to_csv(Path("results", "roc_curve.csv"), index=False)
 
